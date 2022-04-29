a200c57feb1e5be3d1779b4e00954a0633460189
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-04-28 21:46:52 -0300

    Add files via upload

---
+++ function_join_merge.R
@@ -0,0 +1,38 @@
+
+# Juntando bases de dados ------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco --------------------------------------------------------------------------------------------------------
+# Data 28/04/2022 --------------------------------------------------------------------------------------------------------------------------
+# Referência: https://www.youtube.com/watch?v=JiIHJN2TM7g ----------------------------------------------------------------------------------
+
+# Merge/Join -------------------------------------------------------------------------------------------------------------------------------
+
+### Merge permite unir duas bases de dados com base em uma identificação (chave-ID).
+
+### Existem vários tipos de merge:
+
+# - left join
+# - right join
+# - inner join
+# - anti join
+# - full join
+
+# Left join --------------------------------------------------------------------------------------------------------------------------------
+
+### Retorna todos os elementos comuns nas duas tabelas (base de dados) e os restantes
+### dos elementos na tabela à esquerda.
+
+# Right join -------------------------------------------------------------------------------------------------------------------------------
+
+### Retorna todas as linhas comuns nas duas tabelas e todas as restantes linhas na
+### tabela à esquerda.
+
+# Inner join -----------------------------------------------------------------------------------------------------------------------------
+
+### Retorna todas as linhas comuns as duas bases de dados.
+
+# Full join --------------------------------------------------------------------------------------------------------------------------------
+
+### Retorna todas as linhas das tabelas unidas sejam elas correspondentes ou não.
+
+
+
